f4f4f360834a1fe6a98a5bf7dbd2f0e3123d6825
Wadzim <wadzim.dev@gmail.com>
2022-08-24 22:31:20 +0300

    feat: finish task

--- js/script.js
+++ js/script.js
@@ -1,18 +1,5 @@
-/* Задания на урок:
-
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
-
-2) Изменить жанр фильма, поменять "комедия" на "драма"
-
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
-
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
-
-5) Добавить нумерацию выведенных фильмов */
-
 'use strict';
+
 
 const movieDB = {
     movies: [
@@ -29,23 +16,56 @@ const promoCont = document.querySelector('.promo__content');
 const promoGenre = promoCont.querySelector('.promo__genre');
 const promoBg = promoCont.querySelector('.promo__bg');
 const promoMovieList = promoCont.querySelector('.promo__interactive-list');
+const form = promoCont.querySelector('form');
+const input = form.querySelector('input');
+const inputCheck = form.querySelector('input[type="checkbox"]');
+
+let trashes;
+
+
+buildList();
+form.addEventListener('submit',e => {
+    e.preventDefault();
+    let movie = input.value[0].toUpperCase() + input.value.toLowerCase().slice(1);
+    if (movie.length > 21) {
+        movie = movie.slice(0,21) + '...';
+    }
+    movieDB.movies.push(movie);
+    console.log(movieDB.movies);
+    console.log(inputCheck.checked);
+    if (inputCheck.checked) {
+        console.log('fav movie');
+    }   
+    form.reset();
+    buildList();
+})
+
+
+
 
 promoAdv.forEach(e => {
     e.remove();
 });
 promoGenre.textContent = 'драма';
 promoBg.style.backgroundImage = 'url(img/bg.jpg)';
-promoMovieList.innerHTML = '';
 
-movieDB.movies.sort();
-movieDB.movies.forEach((e, i) => {
-    promoMovieList.innerHTML += `    
-    <li class="promo__interactive-item">${i+1}. ${e}
-        <div class="delete"></div>
-    </li>
-    
-    `
-});
+function buildList() {
+    promoMovieList.innerHTML = '';
+    movieDB.movies.sort();
+    movieDB.movies.forEach((e, i) => {
+        promoMovieList.innerHTML += `    
+        <li class="promo__interactive-item">${i+1}. ${e}
+            <div class="delete"></div>
+        </li>
+        `
+    });
+    trashes = promoCont.querySelectorAll('.delete');
+    trashes.forEach((trash, i) => {
+        trash.addEventListener('click', () => {
+            movieDB.movies.splice(i, 1);
+            buildList();
+        });
+    });    
+}
 
 
-// promo__interactive-item <div class="delete"></div>
